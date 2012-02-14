b1082cf02966e8e61e6c71c60d58c52083fb5f8e
Istvan Demeter <demeter.istvan@ezit.hu>
2012-02-14 16:30:30 +0100

    strip it

--- Modulefile
+++
@@ -1,9 +0,0 @@
-name 'dhoppe-sudo'
-version '0.0.3'
-
-author 'Dennis Hoppe'
-license 'GPLv2'
-project_page 'http://github.com/dhoppe/puppet-sudo'
-source 'git://github.com/dhoppe/puppet-sudo'
-summary 'Sudo Module'
-description 'Sudo Module for Debian, Ubuntu'
--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,7 @@ define sudo::service() {
 		owner   => root,
 		group   => root,
 		mode    => 0440,
-		source  => "puppet:///modules/sudo/common/etc/sudoers.d/${name}",
+		source  => "puppet:///modules/sudo/etc/sudoers.d/${name}",
 		require => Package["sudo"],
 	}
 }
